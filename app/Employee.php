5696a1af41bb47b64f5bc78f83e941ce963e2ee1
Md. Azharul Islam <azharul@atilimited.net>
2019-04-06 10:55:50 +0600

    miller employee info

--- app/Employee.php
+++ app/Employee.php
@@ -12,13 +12,13 @@ class Employee extends Model
 
 
     public static function insertMillerEmployeeInfo($request){
-        $EmployeeInfoId = DB::table('tsm_qc_info')->insertGetId([
+        $EmployeeInfoId = DB::table('ssm_millemp_info')->insertGetId([
             'MILL_ID' => $request->input('MILL_ID'),
-            'LABORATORY_FLG' => $request->input('LABORATORY_FLG'),
-            'SOP_DESC' => $request->input('SOP_DESC'),
-            'IODINE_CHECK_FLG' => $request->input('IODINE_CHECK_FLG'),
-            'MONITORING_FLG' => $request->input('MONITORING_FLG'),
-            'LAB_MAN_FLG' => $request->input('LAB_MAN_FLG'),
+            'TOTMALE_EMP' => $request->input('TOTMALE_EMP'),
+            'TOTFEM_EMP' => $request->input('TOTFEM_EMP'),
+            'FULLTIMEMALE_EMP' => $request->input('FULLTIMEMALE_EMP'),
+            'FULLTIMEFEM_EMP' => $request->input('FULLTIMEFEM_EMP'),
+            'PARTTIMEMALE_EMP' => $request->input('PARTTIMEMALE_EMP'),
             'LAB_PERSON' => $request->input('LAB_PERSON'),
             'REMARKS' => $request->input('REMARKS'),
              'ENTRY_BY' => Auth::user()->id,
