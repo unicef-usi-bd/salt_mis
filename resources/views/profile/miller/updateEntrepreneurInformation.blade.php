75a956f07cd8a743fd50fa75f3d5cdc1c169bd9b
Md. Azharul Islam <azharul@atilimited.net>
2019-04-13 17:42:55 +0600

    view page all data fetching for miller profile

---
+++ resources/views/profile/miller/updateEntrepreneurInformation.blade.php
@@ -0,0 +1,157 @@
+<div id="entrepreneur" class="tab-pane fade">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="alert alert-info entrepreneur_msg"></div>
+
+            {{--<form action="{{ url('/entrepreneur-info') }}" method="post" class="form-horizontal" role="form">--}}
+            <form id="entrepreneurId"  class="form-horizontal" role="form" action="{{ url('edit-entrepreneur-info') }}" >
+                @csrf
+                @if(isset($millerInfoId))
+                    <input type="hidden" value="{{ $millerInfoId }}" name="MILL_ID">
+                @endif
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Registration Type</b></label>
+                        <div class="col-sm-8">
+                                                    <span class="block input-icon input-icon-right">
+                                                       <select id="REG_TYPE_ID" class="chosen-select chosen-container" name="REG_TYPE_ID" data-placeholder="Select or search data">
+                                                           <option value=""></option>
+                                                            @foreach($registrationType as $row)
+                                                               <option value="{{ $row->LOOKUPCHD_ID }}" @if($editEntrepData->REG_TYPE_ID==$row->LOOKUPCHD_ID) selected @endif>{{ $row->LOOKUPCHD_NAME }}</option>
+                                                           @endforeach
+
+                                                       </select>
+                                                    </span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-md-6">
+
+                    <div class="form-group" >
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Type of Owner</b></label>
+                        <div class="col-sm-8">
+                                                    <span class="block input-icon input-icon-right">
+                                                        <select id="OWNER_TYPE_ID" name="OWNER_TYPE_ID" class="chosen-select chosen-container" data-placeholder="Select or search data">
+                                                            <option value=""></option>
+                                                            @foreach($ownerType as $row)
+                                                                <option value="{{ $row->LOOKUPCHD_ID }}" @if($editEntrepData->OWNER_TYPE_ID==$row->LOOKUPCHD_ID) selected @endif>{{ $row->LOOKUPCHD_NAME }}</option>
+                                                            @endforeach
+
+                                                        </select>
+                                                    </span>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <table class="table table-bordered fundAllocation" style="margin-top: 64px;">
+                    <thead>
+                    <tr>
+                        <th style="width:130px ;">Owner Name<span style="color:red;"> *</span></th>
+                        <th style="width:130px ;">Division<span style="color:red;"> </span></th>
+                        <th style="width: ;">District</th>
+                        <th style="width: ;">Upazila</th>
+                        <th style="width: 100px;">Union</th>
+                        <th style="width: ;" >NID</th>
+                        <th style="width: ;">Mobile 1</th>
+                        <th  style="width: ;">Mobile 2</th>
+                        <th  style="width: ;">Email</th>
+                        <th  style="width: ;">Remarks</th>
+                        <th style="width: 30px;"><span class="btn btn-primary btn-sm pull-right rowAdd"><i class="fa fa-plus"></i></span></th>
+                    </tr>
+                    </thead>
+                    <tbody class="newRow">
+                    @foreach($getEntrepreneurRowData as $editEntrepData)
+                        <tr class="rowFirst">
+                            <td>
+                                <span class="budget_against_code hidden"><!-- Drop Total Budget here By Ajax --></span>
+                                <span class="block input-icon input-icon-right">
+                                                        <input type="text" name="OWNER_NAME[]" id="inputSuccess " value="{{ $editEntrepData->OWNER_NAME }}" class="width-100 OWNER_NAME"  />
+                                                    </span>
+                            </td>
+                            <td>
+                                                    <span class="block input-icon input-icon-right">
+                                                        <select class="form-control chosen-select DIVISION_ID" id="ENT_DIVISION_ID" name="DIVISION_ID[]"  >
+                                                            <option value="">Select</option>
+                                                            @foreach($getDivision as $row)
+                                                                <option value="{{$row->DIVISION_ID}}" @if($editEntrepData->DIVISION_ID==$row->DIVISION_ID) selected @endif> {{$row->DIVISION_NAME}}</option>
+                                                            @endforeach
+                                                        </select>
+                                                    </span>
+                            </td>
+                            <td>
+                                                    <span class="block input-icon input-icon-right">
+                                                        <select class="form-control chosen-select ent_district" id="ENT_DISTRICT_ID" name="DISTRICT_ID[]"  >
+                                                            <option value="{{ $editEntrepData->DISTRICT_ID }}">{{ $editEntrepData->DISTRICT_NAME }}</option>
+                                                         </select>
+                                                    </span>
+                            </td>
+                            <td>
+                                                    <span class="block input-icon input-icon-right">
+                                                        <select class="form-control chosen-select ent_upazila" id="ENT_UPAZILA_ID" name="UPAZILA_ID[]"  >
+                                                            <option value="{{ $editEntrepData->DISTRICT_ID }}">{{ $editEntrepData->DISTRICT_NAME }}</option>
+                                                        </select>
+                                                    </span>
+                            </td>
+                            <td>
+                                                    <span class="block input-icon input-icon-right">
+                                                        <select class="form-control ent_union" id="UNION_ID" name="UNION_ID[]"  >
+                                                            <option value="{{ $editEntrepData->DISTRICT_ID }}">{{ $editEntrepData->DISTRICT_NAME }}</option>
+                                                        </select>
+                                                    </span>
+                            </td>
+                            <td>
+                                <span class="budget_against_code hidden"><!-- Drop Total Budget here By Ajax --></span>
+                                <span class="block input-icon input-icon-right">
+                                                        <input type="text" name="NID[]" id="inputSuccess total_amount" value="{{ $editEntrepData->NID }}" class="width-100 NID"  />
+                                                    </span>
+                            </td>
+                            <td>
+                                <span class="budget_against_code hidden"><!-- Drop Total Budget here By Ajax --></span>
+                                <span class="block input-icon input-icon-right">
+                                                        <input type="text" name="MOBILE_1[]" id="inputSuccess total_amount" value="{{ $editEntrepData->MOBILE_1 }}" class="width-100 MOBILE_1"  />
+                                                    </span>
+                            </td>
+                            <td>
+                                <span class="budget_against_code hidden"><!-- Drop Total Budget here By Ajax --></span>
+                                <span class="block input-icon input-icon-right">
+                                                        <input type="text" name="MOBILE_2[]" id="inputSuccess total_amount" value="{{ $editEntrepData->MOBILE_2 }}" class="width-100 MOBILE_2"  />
+                                                    </span>
+                            </td>
+                            <td>
+                                                    <span class="block input-icon input-icon-right">
+                                                        <input type="text" name="EMAIL[]" id="inputSuccess batch_no" value="{{ $editEntrepData->EMAIL }}" class="width-100 EMAIL"  />
+                                                        {{--<input type="hidden" class="batch_disabled" disabled="disabled" name="batch_no[]" value="">--}}
+                                                    </span>
+                            </td>
+                            <td>
+                                <span class="budget_against_code "><!-- Drop Total Budget here By Ajax --></span>
+                                <span class="block input-icon input-icon-right">
+                                                        <input type="text" name="REMARKS[]" id="inputSuccess total_amount" value="{{ $editEntrepData->REMARKS }}" class="width-100 REMARKS"  />
+                                                    </span>
+                            </td>
+                            <td><span class="btn btn-danger btn-sm pull-right rowRemove"><i class="fa fa-remove"></i></span></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <hr>
+                <div class="clearfix">
+                    <div class="col-md-offset-3 col-md-9" style="margin-left: 44%!important;">
+                        <button type="reset" class="btn">
+                            <i class="ace-icon fa fa-undo bigger-110"></i>
+                            {{ trans('dashboard.reset') }}
+                        </button>
+                        <button type="button" class="btn btn-primary btnUpdateEntrepInfo" onclick="entrepreneurTab()">
+                            <i class="ace-icon fa fa-check bigger-110"></i>
+                            Update & Next
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
---
+++ resources/views/profile/miller/updateMillInformation.blade.php
@@ -0,0 +1,244 @@
+<div id="mill" class="tab-pane fade ">
+    <div class="row">
+        <div class="col-md-12">
+
+            <div class="alert alert-info millInfo_msg"></div>
+
+            <form id="millId"  class="form-horizontal" role="form" action="{{ url('edit-mill-info') }}" >
+                {{--<form id="millId"  class="form-horizontal" role="form" action="{{ url('update-mill-information') }}" >--}}
+                <input type="hidden" value="{{ $millerInfoId }}" name="MILL_ID" class="millerInfoId" >
+                @csrf
+
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Name of Mill</b></label>
+                        <div class="col-sm-8">
+                            <span class="block input-icon input-icon-right">
+                               <input type="text" name="MILL_NAME" class="chosen-container mill" value="{{ $editMillData->MILL_NAME }}">
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Process Type</b></label>
+                        <div class="col-sm-8">
+                            <span class="block input-icon input-icon-right">
+                               <select id="REG_TYPE_ID" class="chosen-select chosen-container" name="PROCESS_TYPE_ID" data-placeholder="Select">
+                                   <option value=""></option>
+                                    @foreach($processType as $row)
+                                       <option value="{{ $row->LOOKUPCHD_ID }}" @if($editMillData->PROCESS_TYPE_ID==$row->LOOKUPCHD_ID) selected @endif>{{ $row->LOOKUPCHD_NAME }}</option>
+                                   @endforeach
+
+                               </select>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Type of Mill</b></label>
+                        <div class="col-sm-8">
+                            <span class="block input-icon input-icon-right">
+                               <select disabled="true" id="MILL_TYPE_IDD" class="chosen-select chosen-container" name="MILL_TYPE_ID" data-placeholder="Select">
+                                   <option value=""></option>
+                                    @foreach($millType as $row)
+                                       <option  value="{{ $row->UD_ID }}" @if($editMillData->MILL_TYPE_ID==$row->UD_ID) selected @endif>{{ $row->LOOKUPCHD_NAME }}</option>
+                                   @endforeach
+
+                               </select>
+                            </span>
+                        </div>
+                    </div>
+                    {{--to update type of mill  remove disabled="true" --}}
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Capacity</b></label>
+                        <div class="col-sm-8">
+                                <span class="block input-icon input-icon-right">
+                                   <select id="REG_TYPE_ID" class="chosen-select chosen-container" name="CAPACITY_ID" data-placeholder="Select">
+                                       <option value=""></option>
+                                        @foreach($capacity as $row)
+                                           <option value="{{ $row->LOOKUPCHD_ID }}" @if($editMillData->CAPACITY_ID==$row->LOOKUPCHD_ID) selected @endif>{{ $row->LOOKUPCHD_NAME }}</option>
+                                       @endforeach
+
+                                   </select>
+                                </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Zone</b></label>
+                        <div class="col-sm-8">
+                                <span class="block input-icon input-icon-right">
+                                   <select disabled="true" id="ZONE_IDD" class="chosen-select chosen-container" name="ZONE_ID" data-placeholder="Select">
+                                       <option value=""></option>
+                                        @foreach($getZone as $row)
+                                           <option value="{{ $row->ZONE_CODE }}" @if($editMillData->ZONE_ID==$row->ZONE_CODE) selected @endif>{{ $row->ZONE_NAME }}</option>
+                                       @endforeach
+
+                                   </select>
+                                </span>
+                        </div>
+                    </div>
+                    {{--to update  zone  code remove disabled="true" --}}
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Millers ID</b></label>
+                        <div class="col-sm-8">
+                            <span class="block input-icon input-icon-right">
+                               <input readonly type="text" name="MILLERS_ID" class="chosen-container millersIdd" value="{{ $editMillData->MILLERS_ID }}">
+                            </span>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+                <div class="col-md-6">
+
+                    <div class="form-group" >
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Division</b></label>
+                        <div class="col-sm-8">
+                            <span class="block input-icon input-icon-right">
+                                <select id="DIVISION_ID_MILL" name="DIVISION_ID" class="chosen-select chosen-container division" data-placeholder="Select">
+                                    <option value=""></option>
+                                    @foreach($getDivision as $row)
+                                        <option value="{{ $row->DIVISION_ID }}" @if($editMillData->DIVISION_ID==$row->DIVISION_ID) selected @endif>{{ $row->DIVISION_NAME }}</option>
+                                    @endforeach
+
+                                </select>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>District</b></label>
+                        <div class="col-sm-8">
+                            <span class="block input-icon input-icon-right">
+                               <select id="DISTRICT_ID_MILL" class="chosen-select chosen-container district_mill" name="DISTRICT_ID" data-placeholder="Select">
+                                   <option value="{{ $editMillData->DISTRICT_ID }}">{{ $editMillData->DISTRICT_NAME }}</option>
+                               </select>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Upazila</b></label>
+                        <div class="col-sm-8">
+                            <span class="block input-icon input-icon-right">
+                               <select id="UPAZILA_ID_MILL" class="chosen-select chosen-container upazila_mill" name="UPAZILA_ID" data-placeholder="Select">
+                                   <option value="{{ $editMillData->UPAZILA_ID }}">{{ $editMillData->UPAZILA_NAME }}</option>
+                               </select>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Union</b></label>
+                        <div class="col-sm-8">
+                            <span class="block input-icon input-icon-right">
+                               <select id="UNION_ID_MILL" class="chosen-select chosen-container union_mill" name="UNION_ID" data-placeholder="Select">
+
+                                   <option value="{{ $editMillData->UNION_ID }}">{{ $editMillData->UNION_NAME }}</option>
+                                </select>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Active Status</b></label>
+                        <div class="col-sm-8">
+                            <span class="block input-icon input-icon-right">
+                               <select id="ACTIVE_FLG" class="chosen-select chosen-container" name="ACTIVE_FLG" data-placeholder="Select">
+                                       @if(isset($editMillData))
+                                       <option value="1" @if($editMillData->ACTIVE_FLG=='1') selected  @endif >Active</option>
+                                       <option value="0" @if($editMillData->ACTIVE_FLG=='0') selected  @endif >Inactive</option>
+                                   @else
+                                       <option value="1">Active</option>
+                                       <option value="0">Inactive</option>
+                                   @endif
+                               </select>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputSuccess" class="col-sm-3 control-label no-padding-right" for="form-field-1-1"><b>Remarks</b></label>
+                        <div class="col-sm-8">
+                            <span class="block input-icon input-icon-right">
+
+                               <input type="text" name="REMARKS" value="{{ $editMillData->REMARKS }}" class="chosen-container">
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <hr>
+                <div class="clearfix">
+                    <div class="col-md-offset-3 col-md-9" style="margin-left: 44%!important;">
+                        <button type="reset" class="btn">
+                            <i class="ace-icon fa fa-undo bigger-110"></i>
+                            {{ trans('dashboard.reset') }}
+                        </button>
+                        <button type="button" class="btn btn-primary btnUpdateMillInfo" onclick="millTab()">
+                            <i class="ace-icon fa fa-check bigger-110"></i>
+                            Update & Next
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div> {{--col-md-12--}}
+    </div>
+</div>
+
+{{--for Mill on change division --}}
+<script>
+    $(document).ready(function () {
+        $('select#DIVISION_ID_MILL').on('change',function(){
+            var divisionId = $(this).val(); alert(divisionId);//exit();
+            var option = '<option value="">Select District</option>';
+            var url = 'supplier-profile/get-district/{id}';
+            $.ajax({
+                type : "get",
+                url  : url,
+                data : {'divisionId': divisionId},
+                success:function (data) {
+                    for (var i = 0; i < data.length; i++){
+                        option = option + '<option value="'+ data[i].DISTRICT_ID +'">'+ data[i].DISTRICT_NAME+'</option>';
+                    }
+                    $('.district_mill').html(option);
+                    $('.district_mill').trigger("chosen:updated");
+                }
+            });
+        });
+    });
+
+    $(document).ready(function () {
+        $('select#DISTRICT_ID_MILL').on('change',function(){
+            var districtId = $(this).val(); //alert(districtId); exit();
+            var option = '<option value="">Select Upazila</option>';
+            $.ajax({
+                type : "get",
+                url  : "supplier-profile/get-upazila/{id}",
+                data : {'districtId': districtId},
+                success:function (data) {
+                    for (var i = 0; i < data.length; i++){
+                        option = option + '<option value="'+ data[i].UPAZILA_ID +'">'+ data[i].UPAZILA_NAME+'</option>';
+                    }
+                    $('.upazila_mill').html(option);
+                    $('.upazila_mill').trigger("chosen:updated");
+                }
+            });
+        });
+    });
+
+    $(document).ready(function () {
+        $('#UPAZILA_ID_MILL').on('change',function(){
+            var upazilaId = $(this).val(); //alert(upazilaId);exit();
+            var option = '<option value="">Select Union</option>';
+            $.ajax({
+                type : "get",
+                url  : "supplier-profile/get-union/{id}",
+                data : {'upazilaId': upazilaId},
+                success:function (data) {
+                    for (var i = 0; i < data.length; i++){
+                        option = option + '<option value="'+ data[i].UNION_ID +'">'+ data[i].UNION_NAME+'</option>';
+                    }
+                    $('.union_mill').html(option);
+                    $('.union_mill').trigger("chosen:updated");
+                }
+            });
+        });
+    });
+</script>
