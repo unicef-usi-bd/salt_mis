68754b0c8ba671f66871b249808bc5cced886f04
Jalal <jalal@atilimited.net>
2019-05-07 15:01:28 -0800

    route

--- app/Report.php
+++ app/Report.php
@@ -176,4 +176,18 @@ class Report extends Model
         return $qcReports->get();
     }
 
+    public static function getPurchaseChemicalItemList($centerId){
+        $chemicalItemList = DB::table("smm_item");
+        $chemicalItemList->select('ssc_lookupchd.LOOKUPCHD_NAME', 'smm_item.ITEM_NO','smm_item.ITEM_NAME','tmm_itemstock.*' );
+        $chemicalItemList->leftJoin('ssc_lookupchd','ssc_lookupchd.LOOKUPCHD_ID','=','smm_item.ITEM_TYPE');
+        $chemicalItemList->leftJoin('tmm_itemstock','tmm_itemstock.ITEM_NO','=','smm_item.ITEM_NO');
+        $chemicalItemList->Where('tmm_itemstock.TRAN_TYPE','=','CP');
+        $chemicalItemList->Where('tmm_itemstock.TRAN_FLAG','=','PR');
+
+        if($centerId){
+            $chemicalItemList->Where('tmm_itemstock.center_id','=',$centerId);
+        }
+        return $chemicalItemList->get();
+    }
+
 }
