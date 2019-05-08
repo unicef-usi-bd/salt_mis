ff55d1f995e6991d5cf92a9be52b0e78fd4f9488
Jalal <jalal@atilimited.net>
2019-05-08 13:57:35 -0800

    salt monitor

--- app/Report.php
+++ app/Report.php
@@ -136,6 +136,29 @@ class Report extends Model
                                         GROUP BY b.LOOKUPCHD_NAME, b.ITEM_NO, b.ITEM_NAME"));
     }
 
+    public static function getStockSaltForMiller($centerId,$starDate, $endDate){
+        return DB::select(DB::raw("SELECT b.LOOKUPCHD_NAME, b.ITEM_NO, b.ITEM_NAME, SUM(b.purchase) purchase, SUM(b.reduce) reduce, SUM(b.QTY) STOCK_QTY
+                                        FROM
+                                         (SELECT c.LOOKUPCHD_NAME, i.ITEM_NO, i.ITEM_NAME, s.QTY,
+                                            CASE WHEN s.TRAN_FLAG = 'WS' AND s.TRAN_TYPE = 'S' THEN
+                                                s.QTY
+                                            END reduce,
+                                        
+                                            CASE WHEN s.TRAN_FLAG = 'PR' AND s.TRAN_TYPE = 'SP' THEN
+                                                s.QTY
+                                            END purchase,
+                                            s.TRAN_DATE, s.center_id
+                                            FROM smm_item i, tmm_itemstock s, ssc_lookupchd c
+                                            WHERE i.ITEM_NO = s.ITEM_NO
+                                            AND c.LOOKUPCHD_ID = i.ITEM_TYPE
+                                            AND i.item_type = 26
+                                            AND s.TRAN_FLAG NOT IN ('WR','II')
+                                            AND s.TRAN_TYPE NOT IN ('W','I')) b
+                                            WHERE DATE(b.TRAN_DATE) BETWEEN '$starDate' AND '$endDate'
+                                            AND  b.center_id = $centerId
+                                        GROUP BY b.LOOKUPCHD_NAME, b.ITEM_NO, b.ITEM_NAME"));
+    }
+
     public static function getProcessStock(){
         return DB::select(DB::raw("select w.BATCH_NO, st.QTY,
       case when st.TRAN_TYPE = 'W' then
@@ -299,6 +322,16 @@ class Report extends Model
                                           WHERE DATE(it.TRAN_DATE) BETWEEN '$starDate' AND '$endDate'
                                           and it.center_id = $centerId"));
 
+    }
+
+    public static function monitorSaltSupplierList($starDate,$endDate){
+        $centerId = Auth::user()->center_id;
+      return DB::select(DB::raw("select si.TRADING_NAME, si.TRADER_NAME, lc.LOOKUPCHD_NAME, it.QTY
+	  from ssm_supplier_info si
+	  left join ssc_lookupchd lc on lc.LOOKUPCHD_ID = si.SUPPLIER_TYPE_ID
+	  left join tmm_itemstock it on it.SUPP_ID_AUTO = si.SUPP_ID_AUTO
+      WHERE DATE(it.TRAN_DATE) BETWEEN '$starDate' AND '$endDate'
+	  where it.center_id ='$centerId'"));
     }
 
 }
---
+++ resources/views/reportPdf/purchaseSaltstockMillerReportPdf.blade.php
@@ -0,0 +1,31 @@
+<style>
+    p{
+        line-height: 0.20;
+        font-size: 15px;
+    }
+</style>
+
+<div style="margin-bottom: 15px;text-align: center;">
+    <p>Purchase Salt Stock</p>
+</div><!-- /.row -->
+
+
+<table width="700px" border="1"  style="font-size: 12px; text-align: center;border-collapse: collapse;">
+    <tr>
+        <td class="fixedWidth"> {{ trans('dashboard.sl') }}</td>
+        <th>Item Name</th>
+        <th>Total Stock Amount</th>
+
+    </tr>
+    <?php $sl = 0; ?>
+
+    @foreach($purchaseTotalSaltStock as $purchaseSalt)
+        <tr>
+            <td>{{ ++$sl }}</td>
+            <td>{{$purchaseSalt->ITEM_NAME}}</td>
+            <td>{{$purchaseSalt->STOCK_QTY}}</td>
+
+        </tr>
+    @endforeach
+
+</table>
