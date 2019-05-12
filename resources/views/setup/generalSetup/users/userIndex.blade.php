18cdf9cf0feadfd32224b3b05f93840c09f1f634
Jalal <jalal@atilimited.net>
2019-05-12 14:50:38 -0800

    show profile user wise

--- resources/views/setup/generalSetup/users/userIndex.blade.php
+++ resources/views/setup/generalSetup/users/userIndex.blade.php
@@ -43,64 +43,8 @@
                 <tbody>
                 <?php $sl=0;?>
                 @foreach($users as $user)
-                    @if(!empty($user->POSITIONLEVEl) && $user->POSITIONLEVEl< Auth::user()->POSITIONLEVEl )
-                        <?php echo 'hi' ?>
-                        <tr>
-                            <td class="center">{{ ++$sl }}</td>
-                            <td>{{$user->user_full_name}}</td>
-                            <td>{{$user->username}}</td>
-                            <td>{{$user->ASSOCIATION_NAME}}</td>
-                            <td>{{$user->USERGRP_NAME}}</td>
-                            <td>{{$user->UGLEVE_NAME}}</td>
-                            <td>{{$user->email}}</td>
-                            <td>{{$user->remarks}}</td>
-                            <td>
-                                @if($user->active_status == 1)
-                                    <span class="label label-sm label-info arrowed arrowed-righ">Active</span>
-                                @else
-                                    <span class="label label-sm label-danger arrowed arrowed-righ">Inactive</span>
-                                @endif
-                            </td>
-                            <td class="row{{ $user->id }}">
-                                <div class="hidden-sm hidden-xs action-buttons">
-                                    @php
-                                        $editPermissionLevel = $previllage->UPDATE;
-                                        $viewPermissionLevel = $previllage->READ;
-                                    @endphp
-                                    @if($viewPermissionLevel == 1)
-                                        <a href="#" id="{{ 'users/'.$user->id }}" class="blue showModalGlobal" data-target=".modal" data-toggle="modal" role="button" data-permission="{{ $viewPermissionLevel }}" modal-size="modal-lg" title="{{ trans('user.view_user') }}">
-                                            <span class="blue">
-                                                <i class="ace-icon fa fa-eye bigger-130"></i>
-                                            </span>
-                                        </a>
-                                    @else
-                                        <a href="#" id="{{ 'users/'.$user->id }}" class="blue showModalGlobal" data-target=".modal" data-toggle="modal" role="button" data-permission="{{ $viewPermissionLevel }}" modal-size="modal-lg" title="{{ trans('user.view_user') }}" style="display: none;">
-                                        <span class="blue">
-                                            <i class="ace-icon fa fa-eye bigger-130"></i>
-                                        </span>
-                                        </a>
-                                    @endif
-                                    @if($editPermissionLevel == 1)
-                                        <a class="green showModalGlobal" id="{{ 'users/'.$user->id.'/edit' }}" data-target=".modal" role="button"  data-toggle="modal" data-permission="{{ $editPermissionLevel }}" modal-size="modal-lg" title="{{ trans('user.edit_user') }}">
-                                            <i class="ace-icon fa fa-pencil bigger-130"></i>
-                                        </a>
-                                    @else
-                                        <a class="green showModalGlobal" id="{{ 'users/'.$user->id.'/edit' }}" data-target=".modal" role="button"  data-toggle="modal" data-permission="{{ $editPermissionLevel }}" modal-size="modal-lg" title="{{ trans('user.edit_user') }}" style="display: none;">
-                                            <i class="ace-icon fa fa-pencil bigger-130"></i>
-                                        </a>
-                                    @endif
-                                    {{--@if($bank->checkDelete == 0)--}}
-                                    @if($previllage->DELETE == 1)
-                                        <a class="red clickForDelete row{{ $user->id }}" data-token="{{ csrf_token() }}" data-action="{{ 'users/'.$user->id }}" role="button" title="{{ trans('user.delete_user') }}">
-                                            <i class="ace-icon fa fa-trash-o bigger-130"></i>
-                                        </a>
-                                    @endif
-                                    {{--@endif--}}
-                                </div>
-                            </td>
-                        </tr>
+                    @if(($user->POSITIONLEVEl!=null && ($user->POSITIONLEVEl< Session::get('POSITIONLEVEl') )) || ($user->id==1 && Auth::user()->id != 1))
                     @else
-                        <?php echo 'hello' ?>
                         <tr>
                             <td class="center">{{ ++$sl }}</td>
                             <td>{{$user->user_full_name}}</td>
