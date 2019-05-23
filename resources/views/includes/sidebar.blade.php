e1a4443cc8a37c5c253a434dda6c3f93a90071a0
rubiyat <rubiyat@atilimited.net>
2019-05-23 12:20:27 +0600

    update apk and qr code

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -103,9 +103,9 @@
     </div>
 
     @if(Session::get('MILL_ID'))
-        <img src="{{ asset("image/qr.png") }}" width="130" height="130" style="margin-left: 30px;margin-top: 45px;"/>
+        <img src="{{ asset("image/qrcode.png") }}" width="130" height="130" style="margin-left: 30px;margin-top: 45px;"/>
         <div style="margin-top: 10px;margin-left: 10px;">
-            <a href="{{ asset("image/apk/unicef_salt_aa.apk") }}" class="btn btn-success"><i class="fa fa-android" style="font-size: 18px;"></i> Download App <i class="fa fa-arrow-circle-o-down " style="font-size: 18px;"></i></a>
+            <a href="{{ asset("image/apk/unicef_salt_aa_v1.0.0.apk") }}" class="btn btn-success"><i class="fa fa-android" style="font-size: 18px;"></i> Download App <i class="fa fa-arrow-circle-o-down " style="font-size: 18px;"></i></a>
         </div>
     @endif
 
