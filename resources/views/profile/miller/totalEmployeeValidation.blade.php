f0309ee93dbbc7a0f819b035f0b49dc4854a7f78
Md. Azharul Islam <azharul@atilimited.net>
2019-05-29 11:37:20 +0600

    total employee validation in miller employee tab

--- resources/views/profile/miller/totalEmployeeValidation.blade.php
+++ resources/views/profile/miller/totalEmployeeValidation.blade.php
@@ -89,6 +89,51 @@
         }
 
     });
+    $(document).on("change",".totalMaleEmp", function () {
+
+        var  totalMaleEmp = $('.totalMaleEmp').val();
+        var  totalFemaleEmp = $('.totalFemaleEmp').val();
+        var  partTimeMaleEmp = $('.partTimeMaleEmp').val();
+        var  partTimeFemaleEmp = $('.partTimeFemaleEmp').val();
+        var  fullTimeMaleEmp = $('.fullTimeMaleEmp').val();
+        var  fullTimeFemaleEmp = $('.fullTimeFemaleEmp').val();
+        var total  = parseInt(totalMaleEmp)+parseInt(totalFemaleEmp);
+        var totalPartTime  = parseInt(partTimeMaleEmp)+parseInt(partTimeFemaleEmp);
+        var totalFullTime  = parseInt(fullTimeMaleEmp)+parseInt(fullTimeFemaleEmp);
+        var totalPartFullEmp = parseInt(totalPartTime)+parseInt(totalFullTime);
+        if(total!=totalPartFullEmp){
+
+            $('.finalSubmit').prop('disabled', true);
+            $('span.error').show();
+        }else{
+            $('.finalSubmit').prop('disabled', false);
+            $('span.error').hide();
+        }
+
+    });
+
+    $(document).on("change",".totalFemaleEmp", function () {
+
+        var  totalMaleEmp = $('.totalMaleEmp').val();
+        var  totalFemaleEmp = $('.totalFemaleEmp').val();
+        var  partTimeMaleEmp = $('.partTimeMaleEmp').val();
+        var  partTimeFemaleEmp = $('.partTimeFemaleEmp').val();
+        var  fullTimeMaleEmp = $('.fullTimeMaleEmp').val();
+        var  fullTimeFemaleEmp = $('.fullTimeFemaleEmp').val();
+        var total  = parseInt(totalMaleEmp)+parseInt(totalFemaleEmp);
+        var totalPartTime  = parseInt(partTimeMaleEmp)+parseInt(partTimeFemaleEmp);
+        var totalFullTime  = parseInt(fullTimeMaleEmp)+parseInt(fullTimeFemaleEmp);
+        var totalPartFullEmp = parseInt(totalPartTime)+parseInt(totalFullTime);
+        if(total!=totalPartFullEmp){
+
+            $('.finalSubmit').prop('disabled', true);
+            $('span.error').show();
+        }else{
+            $('.finalSubmit').prop('disabled', false);
+            $('span.error').hide();
+        }
+
+    });
 
 
 
