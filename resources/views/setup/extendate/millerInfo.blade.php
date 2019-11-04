3eb09d2c2b33155870e859276a94976596a6cbd8
Jalal <jalal@atilimited.net>
2019-11-03 16:29:07 -0800

    extend date by association

--- resources/views/setup/extendate/millerInfo.blade.php
+++ resources/views/setup/extendate/millerInfo.blade.php
@@ -5,6 +5,41 @@
         border: none;
     }
 </style>
+<div class="row">
+    <form id="myform" action="{{ url('/extended-date-update') }}" method="post" class="form-horizontal" role="form">
+     @csrf
+    <div class="col-md-12">
+        <div class="col-md-6">
+            <div class="form-group" style="margin-left: 35px;">
+                <label  class="col-sm-8 control-label no-padding-right" for="form-field-1-1"> <b>Renewing days</b><span style="color: red;"> </span> </label>
+                <div class="col-sm-4">
+                    <input style="width: 50px;" id="renewalDaysId" placeholder="0" type="number" min="0"    name="renewing_days"  class="chosen-container renewingDays" value="0">
+                </div>
+                <input type="hidden" name="MILL_ID" value="{{$millInfo->MILL_ID}}">
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="form-group" style="margin-left: -20%">
+                <label  class="col-sm-6 control-label no-padding-right" for="form-field-1-1"> <b>Extended Date</b><span style="color: red;"> </span> </label>
+                <div class="col-sm-4">
+                    <input type="date" name="renewing_date"  class="expierDateId" value="{{date('Y-m-d',strtotime($millInfo->RENEWING_DATE))}}">
+                </div>
+            </div>
+        </div>
+
+        <div class="clearfix" style="margin-left: 210px;">
+            <div class="col-md-offset-3 col-md-9">
+                <button type="submit" class="btn btn-primary">
+                    <i class="ace-icon fa fa-check bigger-110"></i>
+                    {{ trans('dashboard.submit') }}
+                </button>
+            </div>
+        </div>
+    </div>
+    </form>
+</div>
+<hr>
 <div class="col-md-12">
     <div class="error-container">
        <div class="row">
@@ -15,7 +50,7 @@
                    <tr style="margin-left: 200%;">
                        <th>Renewing Date </th>
                        <th> :</th>
-                       <td class="renewalDate" style="background-color: red;  font-weight: bolder; color: white;">{{date('d-m-Y',strtotime($millInfo->RENEWING_DATE))}}</td>
+                       <td class="renewalDate" style="background-color: red; max-width: 100px; font-weight: bolder; color: white;">{{date('d-m-Y',strtotime($millInfo->RENEWING_DATE))}}</td>
                    </tr>
                    <tr>
                        <th>Mill Name</th>
@@ -23,7 +58,9 @@
                        <td>{{$millInfo->MILL_NAME}}</td>
                        <th>Mill Logo</th>
                        <th> :</th>
-                       <td><img id="output"  style="width: 50px;height: 50px;" src="{{ asset('/'.$millInfo->mill_logo) }}" /></td>
+                       <td>
+                           <img id="output"  style="width: 50px;height: 50px;" src="{{ asset('/'.$millInfo->mill_logo) }}" />
+                       </td>
                    </tr>
 
                    <tr>
@@ -48,18 +85,8 @@
                        <th> :</th>
                        <td>{{ $millInfo->DIVISION_NAME }}, {{ $millInfo->DISTRICT_NAME }},<br> {{ $millInfo->UPAZILA_NAME }}</td>
                    </tr>
-                   {{--<tr>--}}
-                       {{--<th>Zone </th>--}}
-                       {{--<th> :</th>--}}
-                       {{--<td>{{$millInfo->ZONE_NAME}}</td>--}}
-                       {{--<th>Millers ID </th>--}}
-                       {{--<th> :</th>--}}
-                       {{--<td>{{$millInfo->MILLERS_ID}}</td>--}}
-                   {{--</tr>--}}
-
                </table>
            </div>
-           <div class="space"></div>
 
            <div class="col-md-6">
                <div class="col-md-12">
@@ -139,7 +166,9 @@
                         <td>{{ $row->issureName }}</td>
                         <td>{{ date('m-d-Y',strtotime($row->ISSUING_DATE)) }}</td>
                         <td>{{ $row->CERTIFICATE_NO }}</td>
-                        <td><img id="output"  style="width: 50px;height: 50px;" src="{{ asset('/'.$row->TRADE_LICENSE) }}" /></td>
+                        <td>
+                           <a href="{{ url('/'. $row->TRADE_LICENSE ) }}" target="_blank"><img style="width: 50px;height: 50px;" src="{{ url('/'. $row->TRADE_LICENSE ) }}" alt="trade license"  width="20%"></a>
+                        </td>
                         <td>{{ date('m-d-Y',strtotime($row->RENEWING_DATE)) }}</td>
                     </tr>
                 @endforeach
