89053a0383a4d2d9754186a2c902b336a2d5d181
Kartic Gharami <kartic@atilimited.net>
2020-08-27 17:59:29 +0600

    Login page new design 27082020

---
+++ resources/views/layouts/adminLogin_27082020.blade.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8" />
+    <title>LOGIN - UNICEF MIS</title>
+
+    <meta name="description" content="User login page" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+    <!-- bootstrap & fontawesome -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/font-awesome/4.5.0/css/font-awesome.min.css') }}" />
+
+    <!-- text fonts -->
+    <link rel="stylesheet" href="{{ asset('assets/css/fonts.googleapis.com.css') }}" />
+
+    <!-- ace styles -->
+    <link rel="stylesheet" href="{{ asset('assets/css/ace.min.css') }}" />
+
+    <!--[if lte IE 9]>
+    <link rel="stylesheet" href="{{ asset('assets/css/ace-part2.min.css') }}" />
+    <![endif]-->
+    <link rel="stylesheet" href="{{ asset('assets/css/ace-rtl.min.css') }}" />
+
+    <!--[if lte IE 9]>
+    <link rel="stylesheet" href="{{ asset('assets/css/ace-ie.min.css') }}" />
+    <![endif]-->
+
+    <!-- HTML5shiv and Respond.js for IE8 to support HTML5 elements and media queries -->
+
+    <!--[if lte IE 8]>
+    <script src="{{ asset('assets/js/html5shiv.min.js') }}"></script>
+    <script src="{{ asset('assets/js/respond.min.js') }}"></script>
+    <![endif]-->
+
+
+    <style>
+        .footer .footer-inner .footer-content {
+            position: absolute;
+            left: 12px;
+            right: 12px;
+            bottom: 4px;
+            padding: 8px;
+            line-height: 36px;
+            border-top: 0px double #E5E5E5;
+        }
+    </style>
+</head>
+
+<body class="login-layout light-login" style="background-image: url('assets/images/salt_login/salt_bg_photo.jpg');background-repeat: no-repeat;background-size: cover">
+<div class="main-container">
+    <div class="main-content">
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1">
+                <div class="login-container">
+                    <div class="center">
+                        <h3>
+                            {{--<span class="blue" id="id-text2">Universal Salt Iodization Program</span>--}}
+                            <span class="" id="id-text2" style="color: #FFFFFF">UNIVERSAL SALT IODIZATION PROGRAM</span>
+                        </h3>
+                    </div>
+
+                    <div class="space-6"></div>
+
+                    <div class="position-relative">
+                        <div id="login-box" class="login-box visible widget-box no-border">
+                            <div class="widget-body">
+                                <div class="widget-main">
+                                    <div class="center">
+                                        <img src="{{asset('image/unicef-bd.png')}}" class="img-responsive center-block" style=" width: 100px;margin-top:15px">
+                                    </div>
+                                    {{-- Custom alert message --}}
+                                    @include('includes.alert')
+                                    {{-- Custom alert message --}}
+                                    <h4 class="header">
+                                        @if (isset($errors) && count($errors) > 0)
+                                            <i class="ace-icon fa fa-times-circle red"></i>
+                                            <span class="text-danger"> Invalid Email or Password </span>
+                                        @else
+                                            <i class="ace-icon fa fa-coffee green"></i>
+                                            Enter Email Address & Password
+                                        @endif
+                                    </h4>
+
+                                    <div class="space-6"></div>
+
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <fieldset>
+                                            <label class="block clearfix">
+                                                <span class="block input-icon input-icon-right">
+                                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Email Address" required autofocus autocomplete="off">
+                                                    <i class="ace-icon fa fa-user"></i>
+                                                </span>
+                                            </label>
+
+                                            <label class="block clearfix">
+                                                <span class="block input-icon input-icon-right">
+                                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
+                                                    <i class="ace-icon fa fa-lock"></i>
+                                                </span>
+                                            </label>
+
+                                            <div class="space"></div>
+                                            <div class="clearfix" style="padding-bottom: 40px;">
+                                                <label class="inline">
+                                                    <a href="{{ route('password.request') }}">Forgot Password</a>
+
+                                                </label>
+
+                                                <button type="submit" class="width-35 pull-right btn btn-sm btn-primary">
+                                                    <i class="ace-icon fa fa-key"></i>
+                                                    <span class="bigger-110">Login</span>
+                                                </button>
+                                            </div>
+                                            <div class="col-md-12" style="padding-top: 0;">
+                                                <img style="width:110px; height: 40px;" src="{{asset('assets/images/salt_login/ismos_logo_en (2).png')}}" class="img-responsive center-block">
+                                            </div>
+                                        </fieldset>
+                                    </form>
+
+                                </div><!-- /.widget-main -->
+                            </div><!-- /.widget-body -->
+                        </div><!-- /.login-box -->
+
+                        <div id="forgot-box" class="forgot-box widget-box no-border">
+                            <div class="widget-body">
+                                <div class="widget-main">
+                                    <h4 class="header red lighter bigger">
+                                        <i class="ace-icon fa fa-key"></i>
+                                        Retrieve Password
+                                    </h4>
+
+                                    <div class="space-6"></div>
+                                    <p>
+                                        Enter your email and to receive instructions
+                                    </p>
+
+                                    <form>
+                                        <fieldset>
+                                            <label class="block clearfix">
+														<span class="block input-icon input-icon-right">
+															<input type="email" class="form-control" placeholder="Email" />
+															<i class="ace-icon fa fa-envelope"></i>
+														</span>
+                                            </label>
+
+                                            <div class="clearfix">
+                                                <button type="button" class="width-35 pull-right btn btn-sm btn-danger">
+                                                    <i class="ace-icon fa fa-lightbulb-o"></i>
+                                                    <span class="bigger-110">Send Me!</span>
+                                                </button>
+                                            </div>
+                                        </fieldset>
+                                    </form>
+                                </div><!-- /.widget-main -->
+
+                                <div class="toolbar center">
+                                    <a href="#" data-target="#login-box" class="back-to-login-link">
+                                        Back to login
+                                        <i class="ace-icon fa fa-arrow-right"></i>
+                                    </a>
+                                </div>
+                            </div><!-- /.widget-body -->
+                        </div><!-- /.forgot-box -->
+
+                        <div id="signup-box" class="signup-box widget-box no-border">
+                            <div class="widget-body">
+                                <div class="widget-main">
+                                    <h4 class="header green lighter bigger">
+                                        <i class="ace-icon fa fa-users blue"></i>
+                                        New User Registration
+                                    </h4>
+
+                                    <div class="space-6"></div>
+                                    <p> Enter your details to begin: </p>
+
+                                    <form>
+                                        <fieldset>
+                                            <label class="block clearfix">
+														<span class="block input-icon input-icon-right">
+															<input type="email" class="form-control" placeholder="Email" />
+															<i class="ace-icon fa fa-envelope"></i>
+														</span>
+                                            </label>
+
+                                            <label class="block clearfix">
+														<span class="block input-icon input-icon-right">
+															<input type="text" class="form-control" placeholder="Username" />
+															<i class="ace-icon fa fa-user"></i>
+														</span>
+                                            </label>
+
+                                            <label class="block clearfix">
+														<span class="block input-icon input-icon-right">
+															<input type="password" class="form-control" placeholder="Password" />
+															<i class="ace-icon fa fa-lock"></i>
+														</span>
+                                            </label>
+
+                                            <label class="block clearfix">
+														<span class="block input-icon input-icon-right">
+															<input type="password" class="form-control" placeholder="Repeat password" />
+															<i class="ace-icon fa fa-retweet"></i>
+														</span>
+                                            </label>
+
+                                            <label class="block">
+                                                <input type="checkbox" class="ace" />
+                                                <span class="lbl">
+															I accept the
+															<a href="#">User Agreement</a>
+														</span>
+                                            </label>
+
+                                            <div class="space-24"></div>
+
+                                            <div class="clearfix">
+                                                <button type="reset" class="width-30 pull-left btn btn-sm">
+                                                    <i class="ace-icon fa fa-refresh"></i>
+                                                    <span class="bigger-110">Reset</span>
+                                                </button>
+
+                                                <button type="button" class="width-65 pull-right btn btn-sm btn-success">
+                                                    <span class="bigger-110">Register</span>
+
+                                                    <i class="ace-icon fa fa-arrow-right icon-on-right"></i>
+                                                </button>
+                                            </div>
+                                        </fieldset>
+                                    </form>
+                                </div>
+
+                                <div class="toolbar center">
+                                    <a href="#" data-target="#login-box" class="back-to-login-link">
+                                        <i class="ace-icon fa fa-arrow-left"></i>
+                                        Back to login
+                                    </a>
+                                </div>
+                            </div><!-- /.widget-body -->
+                        </div><!-- /.signup-box -->
+                    </div><!-- /.position-relative -->
+                </div>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.main-content -->
+</div><!-- /.main-container -->
+<div class="footer">
+    {{--@include('masterGlobal.modalGlobal')--}}
+    <div class="footer-inner">
+        <div class="footer-content">
+						<span class="bigger-120">
+							<span class="blue bolder">{{ Session::get('orgName') }}</span>
+                            <div>
+                                <div style="float:left; width:350px;"></div>
+                                <div class="clearfix"></div>
+                            </div>
+                            </span>            &nbsp; &nbsp;
+            <span class="action-buttons"></span>
+        </div>
+    </div>
+</div>
+
+<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
+    <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
+</a>
+
+<!-- basic scripts -->
+
+<!--[if !IE]> -->
+<script src="{{ asset('assets/js/jquery-2.1.4.min.js') }}"></script>
+
+<!-- <![endif]-->
+
+<!--[if IE]>
+<script src="{{ asset('assets/js/jquery-1.11.3.min.js') }}"></script>
+<![endif]-->
+<script type="text/javascript">
+    if('ontouchstart' in document.documentElement) document.write("<script src='{{ asset("assets/js/jquery.mobile.custom.min.js") }}'>"+"<"+"/script>");
+</script>
+
+<!-- inline scripts related to this page -->
+<script type="text/javascript">
+    jQuery(function($) {
+        $(document).on('click', '.toolbar a[data-target]', function(e) {
+            e.preventDefault();
+            var target = $(this).data('target');
+            $('.widget-box.visible').removeClass('visible');//hide others
+            $(target).addClass('visible');//show target
+        });
+    });
+
+
+
+    //you don't need this, just used for changing background
+    jQuery(function($) {
+        $('#btn-login-dark').on('click', function(e) {
+            $('body').attr('class', 'login-layout');
+            $('#id-text2').attr('class', 'white');
+            $('#id-company-text').attr('class', 'blue');
+
+            e.preventDefault();
+        });
+        $('#btn-login-light').on('click', function(e) {
+            $('body').attr('class', 'login-layout light-login');
+            $('#id-text2').attr('class', 'grey');
+            $('#id-company-text').attr('class', 'blue');
+
+            e.preventDefault();
+        });
+        $('#btn-login-blur').on('click', function(e) {
+            $('body').attr('class', 'login-layout blur-login');
+            $('#id-text2').attr('class', 'white');
+            $('#id-company-text').attr('class', 'light-blue');
+
+            e.preventDefault();
+        });
+
+    });
+</script>
+</body>
+<script> //This script use for prevent back button after logout to login
+    window.onload = function () {
+        if (typeof history.pushState  === "function") {
+            history.pushState ("jibberish", null, null,"/");
+            window.onpopstate = function () {
+                history.pushState('newjibberish', null, null,"/");
+            };
+        } else {
+            var ignoreHashChange = true;
+            window.onhashchange = function () {
+                if (!ignoreHashChange) {
+                    ignoreHashChange = true;
+                    window.location.hash = Math.random();
+                } else {
+                    ignoreHashChange = false;
+                }
+            };
+        }
+    } </script>
+</html>
